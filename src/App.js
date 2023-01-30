5439f08586db6eac435320241b6972d862408f0a
Janelle Cikanek <janellecikanek@gmail.com>
2023-01-29 23:20:03 -0600

    initial app.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,33 @@
-import "./App.css";
-import React from "react";
-import React, { useState } from "react"
-import Header from './components/Header';
-import About from './pages/About';
-import Project from './pages/Project';
-import Contact from "./pages/Contact"
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import PortfolioContainer from "./components/PortfolioContainer";
-
-const App = () => <PortfolioContainer />;
+import React, { useState } from "react";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Project from "./pages/Project";
+import Contact from "./pages/Contact";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import "/src/components/css/style.css";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('AboutMe');
+  const [currentPage, setCurrentPage] = useState("AboutMe");
 
   const renderPage = () => {
-    if (currentPage === 'About') {
+    if (currentPage === "About") {
       return <About />;
     }
-    if (currentPage === 'Contact') {
+    if (currentPage === "Contact") {
       return <Contact />;
+    }
+    if (currentPage === "Resume") {
+      return <Resume />;
     }
     return <Project />;
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
   return (
-    <div className="App">
-      <Header />
-      <Nav />
-      <div>
-        <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
-        {renderPage()}
-        <Footer />
-      </div>
+    <div>
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
       <Footer />
     </div>
   );
