ca1c135b99cc2dde2b914fb490770ecb3828dd9f
Janelle Cikanek <janellecikanek@gmail.com>
2023-01-29 20:38:15 -0600

    initial nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,34 +3,42 @@ import Header from '../Header';
 import '../css/Header.css';
 import '../css/style.css'
 
-export default function Nav() {
-  const linkStyle = { border: '1px black', padding: '5px' };
+function Nav({ currentPage, handlePageChange }) {
+return (
+ 
+      <div className="headerNav-container"> 
+      <Header/>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <a
+            href="#aboutme"
+            onClick={() => handlePageChange('AboutMe')}
+            className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}
+          >
+            About Me
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#portfolio"
+            onClick={() => handlePageChange('Portfolio')}
+            className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+          >
+            Portfolio
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#resume"
+            onClick={() => handlePageChange('Resume')}
+            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+          >
+            Resume
+          </a>
+        </li>
+      </ul>
+      </div>
+    );
+  }
 
-  return (
-    <nav className="main-header-menu">
-      <section
-        style={{
-          display: 'flex',
-          fontFamily: 'helvetica',
-          flexDirection: 'row',
-          alignItems: 'flex-start',
-          justifyContent: 'flex-start',
-        }}
-      >
-        <div style={linkStyle}>
-          <a href="#aboutMe">About me</a>
-        </div>
-        <div style={linkStyle}>
-          <a href="#">Portfolio</a>
-        </div>
-        <div style={linkStyle}>
-          <a href="#">Contact</a>
-        </div>
-        <div style={linkStyle}>
-          <a href="#">Resume</a>
-        </div>
-      </section>
-    </nav>
-  );
-}
-
+export default Nav;
