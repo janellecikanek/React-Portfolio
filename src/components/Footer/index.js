5ecb4f844e179a0187f1470a4df003314100d0f3
Janelle Cikanek <janellecikanek@gmail.com>
2023-01-29 19:10:01 -0600

    footer links working

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,21 @@
+import React from 'react';
+import '../css/Footer.css'
+
 function Footer() {
-    return <div className="footer">Footer</div>
+
+    return ( 
+    <footer className="footer">
+      <div>
+      <a href="https://github.com/janellecikanek">Github</a>
+      </div>
+      <div>
+      <a href="https://www.linkedin.com/in/janelle-cikanek-3746b58/">LinkedIn</a>
+      </div>
+      <div>
+      <a href="mailto:janellecikanek@gmail.com">Email</a>
+      </div>
+      </footer>
+    );
 }
 
 export default Footer;
